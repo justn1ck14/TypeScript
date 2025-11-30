936eedf0abd535d111180dfbacf8aa3fe63c207c
justn1ck14 <deniabratan@gmail.com>
2025-11-30 20:36:07 +0100

    Initial TypeScript setup

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let userName = "Денис";
+let age = 22;
+let isStudent = true;
+console.log(`Ім'я: ${userName}`);
+console.log(`Вік: ${age}`);
+console.log(`Студент: ${isStudent}`);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let userName: string = "Денис";
+let age: number = 22;
+let isStudent: boolean = true;
+
+console.log(`Ім'я: ${userName}`);
+console.log(`Вік: ${age}`);
+console.log(`Студент: ${isStudent}`);
